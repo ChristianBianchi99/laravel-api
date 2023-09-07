b776bc7f43fa35fb35eee65f14303ef2ec8a34d8
ChristianBianchi99 <bianchi848@gmail.com>
2023-09-07 15:10:19 +0200

    Add guzzle - Create model Lead

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -17,4 +17,19 @@ class ProjectController extends Controller
             'results'   => $projects,
         ]);
     }
+
+    public function show($slug){
+        $project = Project::with('type')->where('slug', $slug)->first();
+
+        if($project){
+            return response()->json([
+                'success'   => true,
+                'results'   => $project,
+            ]);
+        }
+        return response()->json([
+            'success'   => false,
+            'error'     => 'Nessun progetto trovato'
+        ]);
+    }
 }
